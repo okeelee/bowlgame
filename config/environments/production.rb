d371b04cc2c5d0f968e4c917be8687112e999647
Lee O'Keefe <okeefe.lee@gmail.com>
2011-12-12 20:44:29 -0800

    try and set up the mailer on heroku again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Bowlgame::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'] 
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'bowlgame.heroku.com'
+    :domain         => 'bowlgame.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
