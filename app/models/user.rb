bad3bb0e1705748dc084d346d35872910d7ef1a7
Lee O'Keefe <okeefe.lee@gmail.com>
2011-12-13 19:46:09 -0800

    forgot to make the attributes editable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :paid, :admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :paid, :admin, :first_name, :last_name
   
   has_many :picks
   
