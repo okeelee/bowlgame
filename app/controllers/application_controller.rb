8ea72e547bc56606ef513b0715bb8dbb929f0a05
Chris Sinco <csi7585@gmail.com>
2011-12-20 01:22:37 -0800

    adding mobile detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :get_pickem, :authenticate_user!
+  before_filter :get_pickem, :authenticate_user!, :prepare_for_mobile
   
   def is_admin_user?
     redirect_to root_path unless current_user.is_admin?
@@ -13,4 +13,19 @@ class ApplicationController < ActionController::Base
     @pickem = Pickem.find(PICKEM_ID) || Pickem.new
   end
   
+  private
+  
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,9 @@ class HomeController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    
-    @posts = Post.includes(:user_comments).page(params[:page]).per(5)
+    @posts = Post.includes(:user_comments).order('created_at DESC').limit(5)    
     @games_today = BowlGame.includes([{:teams=>:conference}]).where(:game_time => Date.today+8.hours..Date.tomorrow+8.hours).order('game_time')
     @top5 = User.order('score DESC').limit(5)
-    
-    respond_to do |format|
-      format.html # index.html.erb
-    end
   end
   
   private
