1bb214aecf1fda100c7cb9aa0e352905a6039e9c
Chris Sinco <csi7585@gmail.com>
2011-12-20 07:52:03 -0800

    remove mobile detection for now, until all mobile pages are built

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   helper_method :mobile_device?
   
   def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
+    #session[:mobile_param] = params[:mobile] if params[:mobile]
+    #request.format = :mobile if mobile_device?
   end
 end
