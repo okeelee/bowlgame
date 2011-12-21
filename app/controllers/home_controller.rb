241f00ca7eb84ed927a08b10957678d6dc90cd82
Chris Sinco <csi7585@gmail.com>
2011-12-20 19:08:00 -0800

    time zone fix with home controller, removed +8.hours to date range

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   # GET /users.json
   def index
     @posts = Post.includes(:user_comments).order('created_at DESC').limit(5)    
-    @games_today = BowlGame.includes([{:teams=>:conference}, {:picks=>:team}]).where(:game_time=>Date.today+8.hours..Date.tomorrow+8.hours).where(:picks=>{:user_id=>current_user.id}).order('game_time')
+    @games_today = BowlGame.includes([{:teams=>:conference}, {:picks=>:team}]).where(:game_time=>Date.today..Date.today.tomorrow).where(:picks=>{:user_id=>current_user.id}).order('game_time')
     @top5 = User.order('score DESC').limit(5)
   end
 end
